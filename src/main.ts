fd30e991f96dd58a6adca0ec14411db1550dfc69
Глеб Евдокимов <g.evdokimov@jetmail.cc>
2025-07-09 19:39:09 +0300

    configure pinia and add css

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { createApp } from "vue";
-import PrimeVue from "primevue/config";
-import App from "./App.vue";
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
+import PrimeVue from "primevue/config";
+import Aura from "@primeuix/themes/aura";
+
+import App from "./App.vue";
+import "./styles/index.css";
+
+const pinia = createPinia();
 const app = createApp(App);
 
-app.use(PrimeVue);
+app.use(pinia);
+pinia.use(piniaPluginPersistedstate);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
 
 app.mount("#app");
